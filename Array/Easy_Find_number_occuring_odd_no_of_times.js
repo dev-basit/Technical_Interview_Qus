d5d097d3c52fc5b1e19ca469b91b9289da02f86d
Abdul Basit <dev.basit2020@gmail.com>
2022-11-17 18:15:25 +0500

    Qus 4

---
+++ Array/Easy_Find_number_occuring_odd_no_of_times.js
@@ -0,0 +1,74 @@
+//   --------------- Method 1 - O(n^2) time
+/*
+(function () {
+  let nums = [1, 3, 3, 1, 3, 2, 1, 2];
+  let count = 0;
+
+  for (let i of nums) {
+    count = 0;
+    //   for each element repeat again, if you find nums[i]==nums[j], add 1 to count
+    for (let j of nums) if (i === j) count += 1;
+
+    //   if count modulo 2 not = 0, return it
+    if (count % 2 === 1) {
+      console.log(i);
+      return i;
+    }
+  }
+})();
+*/
+
+//   --------------- Method 2 - O(n*logn)
+/*
+(function () {
+  let nums = [1, 2, 2, 4, 2, 2, 1, 2];
+  let length = nums.length;
+  let count = 0;
+
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < length; i++) {
+    if (nums[i] == nums[i - 1]) count += 1;
+    else {
+      if (count % 2 === 1) {
+        console.log(nums[i - 1]);
+        return nums[i - 1];
+      }
+      count = 1;
+    }
+  }
+})();
+*/
+
+//  ---------------- Method 3 - O(n) time & O(k) space
+/*
+(function () {
+  let nums = [2, 2, 4, 2, 3, 2, 3, 4, 4];
+  let allOccurence = {};
+
+  for (let value of nums) {
+    if (!allOccurence[value]) allOccurence[value] = 1;
+    else allOccurence[value] += 1;
+  }
+
+  for (let i in allOccurence) {
+    if (allOccurence[i] % 2 === 1) {
+      console.log("odd occurence no is ", i);
+      break;
+    }
+  }
+})();
+*/
+
+//  ---------------- Method 4 - O(n) time & O(1) space
+/*
+(function () {
+  let nums = [0, 1];
+  let length = nums.length;
+  let xor = nums[0];
+
+  for (let i = 1; i < length; i++) xor ^= nums[i];
+
+  console.log(xor);
+})();
+*/
